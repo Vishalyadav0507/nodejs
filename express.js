2c42e61637bf15d87e8920e7e8f8fc701897ec97
vishalyadav0507 <vishal364364yadav>
2023-02-01 21:01:29 +0530

    express_body_parser

--- express.js
+++ express.js
@@ -1,18 +1,20 @@
-const http=require('http');
 
 const express= require('express')
-
+const bodyParser=require('body-parser')
 const app=express()
 
-app.use((req,res,next)=>{
-    console.log("hello middlewire")
-    next()
-})
+app.use(bodyParser.urlencoded({extended:false}))
 
-app.use((req,res,next)=>{
-    console.log('hii second middleware')
-    res.send( { key1: "value" })
+app.use("/hello",(req,res,next)=>{
+    res.send(`<form action="/get-product" method="POST" ><input type="text" name="title"></input><input type="text" name="size"></input><button type ="submit" >submit</button></form>`)
+    
 })
-const server=http.createServer(app)
-
-server.listen(3000)
+app.use("/get-product",(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+    
+})
+app.use("/",(req,res,next)=>{
+    res.send(`<h2>hello express</h2>`)
+})
+app.listen(3000)
